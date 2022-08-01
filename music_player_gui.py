fff93538585f5980f4140440340de065aa7f4a2b
KonnorFrik <konnor.frik666@gmail.com>
2022-08-01 13:24:14 +0500

    with comment

--- music_player_gui.py
+++ music_player_gui.py
@@ -6,23 +6,26 @@ from kivy.uix.pagelayout import PageLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.slider import Slider
-
 from playlist import *
-# import playlist as music_player
 from time import sleep
 from threading import Thread
+from vlc import State
 
-DEBUG = True
+DEBUG = False
 
 
 class AudioPlayerApp(App):
     # option's for widget's
-    __button_size = (1, 1)
+
+    # for all button
+    __button_size_hint = (1, 1)
     __gif_size = (1, 6)
 
+    # for label with song name
     __song_name_size = (1, 0.5)
     __song_name_font_size = 25
 
+    # for volume slider
     __volume_slider_size = (0, 0)
     __volume_track_color = [1, 0, 0, 1]
     __volume_slider_range_min = 15
@@ -31,12 +34,14 @@ class AudioPlayerApp(App):
     __volume_slider_default_value = 50
     __volume_slider_cursor_size = (0, 0)
 
+    # for song scroll slider (don't use)
     __rewind_track_color = [75 / 255, 90 / 255, 255 / 255, 1]
     __rewind_track_size = (20, 20)
     __rewind_track_step = 1
 
     def __init__(self):
         super().__init__()
+        """Init all variables"""
         self.volume_slider: Slider
         self.next_button: Button
         self.pause_button: Button
@@ -58,19 +63,22 @@ class AudioPlayerApp(App):
 
         self.__create_objs()
         self.__bind()
-
-        self.create_playlists_names_list()
-        self.create_playlists_objects()
-        self.place_playlists_names()
+        
+        # create once a playlist's list for switch  
+        self.__create_playlists_names_list()
+        self.__create_playlists_objects()
+        self.__place_playlists_names()    # place names to page 2
 
     def __create_objs(self):
+        """Create all kivy object's
+            Setup setting's for they"""
         self.name_label = Label(text=self.player.get_song_name(), size_hint=self.__song_name_size,
                                 font_size=self.__song_name_font_size)
         self.gif_pass = Button(text='GIF', size_hint=self.__gif_size)
-        self.previous_button = Button(text='Previous', size_hint=self.__button_size)
-        self.play_button = Button(text='Play', size_hint=self.__button_size)
-        self.pause_button = Button(text='Pause', size_hint=self.__button_size)
-        self.next_button = Button(text='Next', size_hint=self.__button_size)
+        self.previous_button = Button(text='Previous', size_hint=self.__button_size_hint)
+        self.play_button = Button(text='Play', size_hint=self.__button_size_hint)
+        self.pause_button = Button(text='Pause', size_hint=self.__button_size_hint)
+        self.next_button = Button(text='Next', size_hint=self.__button_size_hint)
         self.volume_slider = Slider(min=self.__volume_slider_range_min, max=self.__volume_slider_range_max,
                                     step=self.__volume_slider_step, value=self.__volume_slider_default_value, orientation='vertical',
                                     value_track=True, value_track_color=self.__volume_track_color, cursor_size=self.__volume_slider_cursor_size)
@@ -79,6 +87,7 @@ class AudioPlayerApp(App):
         #                             cursor_size=self.__rewind_track_size, size_hint=(1, 0.25))
 
     def __bind(self):
+        """create bind's for kivy object's"""
         Window.bind(on_request_close=self._to_close)
         self.previous_button.bind(on_press=self.previous_gui)
         self.next_button.bind(on_press=self.next_gui)
@@ -90,6 +99,25 @@ class AudioPlayerApp(App):
         # self.gif_pass.bind(on_press=(lambda *x: self.player.rewind(self.player.song_length - 5_000)))
         # self.rewind_slider.bind(on_touch_up=self.rewind_song)
 
+    def __create_playlists_names_list(self):
+        self.playlists_name = self.player.get_all_playlists_name()
+
+    def __create_playlists_objects(self):
+        # create button object's for playlist's name's 
+        for name in self.playlists_name:
+            item = Button(text=name, size_hint_y=None, height=70)
+            item.bind(on_release=self.switch_playlist)
+            self.playlists_objects.append(item)     # and add them to list
+
+    def __place_playlists_names(self):
+        for obj in self.playlists_objects:
+            self.grid.add_widget(obj)
+
+    # def rewind_song(self, *args):
+    #     pass
+        # self.player.rewind(self.rewind_slider.value)
+        # print(f"{self.rewind_slider.value}")
+
     def build(self):
         page = PageLayout()
         main = BoxLayout(orientation='vertical')
@@ -100,6 +128,7 @@ class AudioPlayerApp(App):
         second.add_widget(self.play_button)
         second.add_widget(self.next_button)
         second.add_widget(self.volume_slider)
+        
         main.add_widget(self.name_label)
         main.add_widget(self.gif_pass)
         # main.add_widget(self.rewind_slider)
@@ -109,54 +138,40 @@ class AudioPlayerApp(App):
         page.add_widget(self.grid)
         return page
 
-    # def rewind_song(self, *args):
-    #     pass
-        # self.player.rewind(self.rewind_slider.value)
-        # print(f"{self.rewind_slider.value}")
-
-    def create_playlists_names_list(self):
-        self.playlists_name = self.player.get_all_playlists_name()
-
-    def create_playlists_objects(self):
-        for name in self.playlists_name:
-            item = Button(text=name, size_hint_y=None, height=70)
-            item.bind(on_release=self.switch_playlist)
-            self.playlists_objects.append(item)
-
-    def place_playlists_names(self):
-        for obj in self.playlists_objects:
-            self.grid.add_widget(obj)
-
     def switch_playlist(self, label_obj: Button, *a):
-        name = label_obj.text
-        self.player.change_playlist(name)
+        """Change playlist to another and start play 1st song in new"""
+        self.player.change_playlist(label_obj.text)
         self._update_song_name()
 
         self.play_gui()     # ???
 
-    # def clear_playlists_name(self):
-    #   Ведь не нужно очищать список всех плей листов??
-    #     self.grid.clear_widgets(self.playlists_objects)
-
     def change_volume_gui(self, *arg):
+        """Change volume"""
+        # for volume slider
         self.player.change_volume(value=int(arg[0].value))
 
     def pause_gui(self, *a):
+        """Pause a current song"""
+        # for pause button
         self.player.pause()
         # self._is_play = False
 
     def play_gui(self, *a):
+        """Start play a current song"""
+        # for play button 
         self.player.play()
         # self._is_play = True
         # sleep(0.01)
         # self.update_song_length()
 
-        if not self.updater.is_alive():
+        if not self.updater.is_alive():     # start thread for check is song end
             self.updater.start()
         # self._update_rewind_max()
         # print(f"rewind value now: {self.rewind_slider.max}")
 
     def next_gui(self, *args):
+        """Choose a next song in playlist and play it"""
+        # for next button
         self.player.next()
         self.play_gui()
         self._update_song_name()
@@ -164,6 +179,8 @@ class AudioPlayerApp(App):
         # self.__set_rewind_max_value()
 
     def previous_gui(self, *args):
+        """Choose a previous song in playlist and play it"""
+        # for previous button
         self.player.previous()
         self.play_gui()
         self._update_song_name()
@@ -171,35 +188,42 @@ class AudioPlayerApp(App):
         # self.__set_rewind_max_value()
 
     def _update_song_name(self):
+        """Show a current name of playing song"""
         self.name_label.text = self.player.get_song_name()
 
     def _to_close(self, *a):
+        """For main window for close thread and correctly close program"""
         self.thread_work = False
 
     def check_to_next(self, *a):
+        """Check song for his ending
+            and choose next"""
+        # for thread
         while True:
-            if not self.thread_work:
+            if not self.thread_work:  # stop thread while close a main window
                 return
-            # print(f"\tPOSITION NOW: {self.player.get_song_position()} / ALL LENGTH: {self.player.song_length}")
-            if self.player.get_state() == vlc.State.Ended:
-                # print("NEXT")
-                self.next_gui()
-            sleep(0.1)
 
-    def update_song_length(self):
-        self.song_length = self.player.song_length
+            if self.player.get_state() == State.Ended:  # check if song is ended 
+                self.next_gui() 
+            sleep(0.1)  # to not overloading the system
+
+    # def update_song_length(self):
+    #     self.song_length = self.player.song_length
         # print(f"LEN: {self.song_length}")
 
     # def _update_rewind_max(self):
+    #     """Reset the max value for rewind slider"""
     #     sleep(0.01)
     #     self.rewind_slider.max = self.player.song_length
 
     # def __set_rewind_max_value(self):
+    #     # ??? 
     #     value = self.player.song_length
-    #     self.rewind_slider.max = value if value > 0 else 100
+    #     self.rewind_slider.max = value if value > 0 else 100  # if song not playing his length is -1 
     #     self.rewind_slider.value = 0
 
     # def update_rewind(self):
+    #     # for thread whom update a value a rewind slider   
     #     while True:
     #         while self._is_play:
     #             print("\t\tupdate")
--- playlist.py
+++ playlist.py
@@ -1,21 +1,23 @@
-import vlc
-
 import music_player
 import os
 
-BASIC_MUSIC_DIR = "/home/konnor/Music/"
+# folder with playlist's folder's
+BASIC_MUSIC_DIR = "/path/to_your/Music/"
 DEBUG = False
 
 
 class PlayListManager(music_player.MusicPlayer):
     def __init__(self):
         super().__init__()
+        # get all playlist's folder names
         self._all_playlists = [file for file in os.listdir(BASIC_MUSIC_DIR) if os.path.isdir(BASIC_MUSIC_DIR+file)]
-        self.current_playlist_name = self._all_playlists[0]
+        self.current_playlist_name = self._all_playlists[0]  # choose 1st playlist for play
 
+        # create song list for current playlist
         self._current_song_list = None
         self.create_playlist(self.current_playlist_name)
 
+        # choose 1st song for play
         self.__song_number = 0
         self._set_new_audio(self._current_song_list[self.__song_number])  # !!!!!!!!!!!
 
@@ -28,8 +30,9 @@ class PlayListManager(music_player.MusicPlayer):
             print('')
 
     def next(self, *args):
+        """Set next audio to play from playlist folder"""
         self.__song_number += 1
-        try:
+        try:    # 'try' for prevent IndexError
             song = self._current_song_list[self.__song_number]
             if DEBUG:
                 print(f"\nin 'try', index for playlist: {self.__song_number} and item from list: {self._current_song_list[self.__song_number]}")
@@ -38,42 +41,46 @@ class PlayListManager(music_player.MusicPlayer):
         except IndexError:
             self.__song_number = 0
             song = self._current_song_list[self.__song_number]
-            self.stop()
+            # self.stop()  # ???
             if DEBUG:
                 print(f"\nin 'except', index for playlist: {self.__song_number} and item from list: {self._current_song_list[self.__song_number]}")
                 print(f"song in 'except' in 'next: {song}")
                 print('')
-        if DEBUG:
-            print('')
         self._set_new_audio(song)
         # self.play()
 
     def previous(self, *args):
+        """Set previous audio to play from playlist folder"""
         self.__song_number -= 1
-        if self.__song_number < 0:
+
+        if self.__song_number < 0:  # song index can't be less 0 to prevent IndexError
             self.__song_number = len(self._current_song_list) - 1
+
         song = self._current_song_list[self.__song_number]
         if DEBUG:
-            print('')
+            print(f"")
         self._set_new_audio(song)
         # self.play()
 
     def get_song_name(self, *args) -> str:
+        """Get current song name"""
         return self._current_song_list[self.__song_number].split('.')[0]
 
-    def get_current_playlist_name(self):
+    def get_current_playlist_name(self) -> str:
         return self.current_playlist_name
 
-    def get_all_playlists_name(self):
+    def get_all_playlists_name(self) -> list[str]:
+        """Get all playlists folder names"""
         return self._all_playlists.copy()
     
-    def get_all_songs_in_playlist(self):
+    def get_all_songs_in_playlist(self) -> list[str]:
         return self._current_song_list
     
     def create_playlist(self, folder_path):
-        if folder_path[-1] != '/':
+        """Create playlist by taking song files names from playlist folder"""
+        if folder_path[-1] != '/':  # add separate
             folder_path += '/'
-        current_folder = BASIC_MUSIC_DIR + folder_path
+        current_folder = BASIC_MUSIC_DIR + folder_path  # make valid path to playlist folder
 
         self._current_song_list = [file for file in os.listdir(current_folder) if os.path.isfile(current_folder + file)]
 
@@ -84,10 +91,11 @@ class PlayListManager(music_player.MusicPlayer):
             print('')
 
     def change_playlist(self, name):
-        self.current_playlist_name = name
+        """Switch to another playlist folder by his name"""
+        self.current_playlist_name = name   # reset current playlist name
         self.create_playlist(self.current_playlist_name)
 
-        self.__song_number = 0
+        self.__song_number = 0  # reset a index
         self._set_new_audio(self._current_song_list[self.__song_number])
 
         if DEBUG:
